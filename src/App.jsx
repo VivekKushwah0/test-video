477aeb595c5fb00bf0904afb8ac4a24e01915c78
Vivek Kushwah <vivek.kushwah@silverpush.co>
2024-10-14 10:48:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <video
+        style={{
+          width: '100%',
+          height: '90vh',
+        }}
+        autoPlay={true}
+        muted
+        controls={true}
+      >
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </>
+  );
+}
+
+export default App;
